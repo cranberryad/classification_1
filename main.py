d9135fb3f944a5826ad95d841b305c6f5a002534
cranberryad <andriy0000000001@gmail.com>
2025-07-23 22:51:07 +0200

    Added and updated main

---
+++ main.py
@@ -0,0 +1,50 @@
+import io
+import keras
+from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
+import ml_edu.experiment
+import ml_edu.results
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+print("Ran the import statements.")
+
+rice_dataset_raw = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/Rice_Cammeo_Osmancik.csv")
+
+rice_dataset = rice_dataset_raw[[
+    'Area',
+    'Perimeter',
+    'Major_Axis_Length',
+    'Minor_Axis_Length',
+    'Eccentricity',
+    'Convex_Area',
+    'Extent',
+    'Class',
+]]
+
+rice_dataset.describe()
+
+print(
+    f'The shortest grain is {rice_dataset.Major_Axis_Length.min():.1f}px long,'
+    f' while the longest is {rice_dataset.Major_Axis_Length.max():.1f}px.'
+)
+print(
+    f'The smallest rice grain has an area of {rice_dataset.Area.min()}px, while'
+    f' the largest has an area of {rice_dataset.Area.max()}px.'
+)
+print(
+    'The largest rice grain, with a perimeter of'
+    f' {rice_dataset.Perimeter.max():.1f}px, is'
+    f' ~{(rice_dataset.Perimeter.max() - rice_dataset.Perimeter.mean())/rice_dataset.Perimeter.std():.1f} standard'
+    f' deviations ({rice_dataset.Perimeter.std():.1f}) from the mean'
+    f' ({rice_dataset.Perimeter.mean():.1f}px).'
+)
+print(
+    f'This is calculated as: ({rice_dataset.Perimeter.max():.1f} -'
+    f' {rice_dataset.Perimeter.mean():.1f})/{rice_dataset.Perimeter.std():.1f} ='
+    f' {(rice_dataset.Perimeter.max() - rice_dataset.Perimeter.mean())/rice_dataset.Perimeter.std():.1f}'
+)
